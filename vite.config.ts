f984273e623f2aa4dcb99764ab3f8e0a8520c66e
fabiozocchi80 <fabio.zocchi.80@gmail.com>
2026-04-22 11:20:42 +0200

    build: Increase chunk size warning limit
    Increased the `chunkSizeWarningLimit` in `vite.config.ts` from 1000 to 2500. This change is necessary to accommodate larger application bundles without triggering unnecessary warnings, which can be distracting during development.

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig(({mode}) => {
       hmr: process.env.DISABLE_HMR !== 'true',
     },
     build: {
-      chunkSizeWarningLimit: 1000,
+      chunkSizeWarningLimit: 2500,
     },
   };
 });
